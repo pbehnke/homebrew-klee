4613ebf77e608fd46a42fcaffe8319cb8d16a054
Phil Behnke <phil@pbehnke.com>
2015-05-04 13:26:44 -0400

    create the correct directory

--- minisat.rb
+++ minisat.rb
@@ -3,7 +3,7 @@ class Minisat < Formula
   head "https://github.com/stp/minisat.git"
 
   def install
-  	Dir.mkdir 'default'
+  	Dir.mkdir 'build'
   	cd 'build' do
   		system "cmake", ".", *std_cmake_args
   		system "make", "install"
